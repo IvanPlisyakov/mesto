b4da1266c7de779f57f80719ca8a6b7cbe66d01c
Ivan Plisyakov <ivan.plisyakov@mail.ru>
2020-08-27 00:50:37 +0700

    2.2.0

--- scripts/script.js
+++ scripts/script.js
@@ -34,33 +34,28 @@ const pictureOpening = document.querySelector('.picture-opening');//секция
 
 //переменные связанные с попапами
 
-const form = document.querySelector(".form"); //форма
+const formEditButton = document.querySelector(".form_edit-button"); //формы
+const formAddButton = document.querySelector(".form_add-button");
+
 const formTitle = document.querySelector(".profile-form__title");  //заголовок формы
-const editButton = document.querySelector(".profile__edit-button"); //создать форму
+const editButton = document.querySelector(".profile__edit-button"); //создать форму  
 const addButton = document.querySelector(".profile__add-button"); //создать форму
 
-const inputName = document.querySelector(".profile-form__user_data_name"); //связываем поля формы с соотв. переменными
-const inputInfo = document.querySelector(".profile-form__user_data_info");
+const inputName = formEditButton.querySelector(".profile-form__user_data_name"); //связываем поля 'edit' формы с соотв. переменными
+const inputInfo = formEditButton.querySelector(".profile-form__user_data_info");
+
+const inputTitle = formAddButton.querySelector(".profile-form__user_data_name"); //связываем поля 'add' формы с соотв. переменными
+const inputLink = formAddButton.querySelector(".profile-form__user_data_info");
 
 const userName = document.querySelector(".profile__user-name");  //связываем данные профиля с переменными
 const userInfo = document.querySelector(".profile__user-info");
 
-let state; //отслеживаем какой попап открыт
+const btnClose = pictureOpening.querySelector('.profile-form__btn-close')
 
 //функции
 
 function smoothCloseOpenPopup(blockName){  /* хоть где-то popup правильно написал... */
-  if(blockName.classList.contains('popap-active') == false){  //если попав не активен то мы убираем закрытие, добавляем открытие и добавляем disaply:block;
-    blockName.classList.remove("popap-close");  //убираем закрытие
-    blockName.classList.add("popap-open");  //добавляем открытие
-    blockName.classList.add("popap-active");  //активируем display
-  }else{
-    blockName.classList.remove("popap-open");  // НЕ убираем закрытие
-    blockName.classList.add("popap-close"); // НЕ добавляем открытие
-    setTimeout(function() {
-      blockName.classList.remove("popap-active");  // НЕ активируем display
-    }, 1000); //задержка для того, чтобы увидеть закрытие
-  }
+  blockName.classList.toggle("popap-active");
 }
 
 function createElement(elementTitle, elementImg) {//чтобы создать карточку нам нужны её title и её image link
@@ -75,88 +70,63 @@ function createElement(elementTitle, elementImg) {//чтобы создать к
     pictureOpening.querySelector('.picture-opening__img').setAttribute('src', elementImg);  //вставляем картинку
 
     smoothCloseOpenPopup(pictureOpening);
-
-    pictureOpening.querySelector('.profile-form__btn-close').addEventListener('click', function () {  //закрытие картинки
-      smoothCloseOpenPopup(pictureOpening);
-    });
   });
 
   elementCopy.querySelector('.element__btn-like').addEventListener('click', function (evt) { /*inst: vanishhhhhhhhh проверьте, пожалуйста, работают ли там лайки тоже*/
     evt.target.classList.toggle('element__btn-like_active');
   });
 
-  elementCopy.querySelector('.element__btn-delete').addEventListener('click', function (evt) {// удаление карточки + изм. количества карточек
+  elementCopy.querySelector('.element__btn-delete').addEventListener('click', function (evt) {// удаление карточки 
     evt.target.closest(".element").remove();//обращаемся к ближайщему родителю ".element" и удаляем его
   });
 
   elements.append(elementCopy);// отображаем на странице
 };
 
+btnClose.addEventListener('click', function () {  //закрытие картинки
+  smoothCloseOpenPopup(pictureOpening);
+});
+
 // функции отвечающие за попапы
 
-function Form(nameButton) {  //создание нужной нам формы
-  switch (nameButton) {
-    case 'editButton':
-      modificationFormEditButton();
-      break;
-    case 'addButton':
-      modificationFormAddButton();
-      break;
-  }
-  state = nameButton;
-  initializationForm();
-}
+function openingEditForm(){
+  smoothCloseOpenPopup(formEditButton);
 
-function initializationForm(){ //создаём основу нашего попапа (которая, пока что, одинаковая)
-  smoothCloseOpenPopup(form);
-}
-
-function modificationFormEditButton(){ //если нажали EditButton, то сделай соотв. изм.
   inputName.value = userName.textContent; //в форме будет показывать то значение, которые было в профиле
   inputInfo.value = userInfo.textContent;
-  formTitle.textContent = "Редактировать профиль";
 }
 
-function modificationFormAddButton(){ //если нажали AddButton, то сделай соотв. изм.
-  inputName.value = ""; //обнуляем введённое значение
-  inputInfo.value = "";
-  inputName.placeholder = "Название";  // здесь значения по умолчанию
-  inputInfo.placeholder = "Ссылка на картинку";
-  formTitle.textContent = "Новое место";
-}
-
-function closeForm() {
-  smoothCloseOpenPopup(form);
-}
-
-function saveForm(event) {
+function saveEditForm(event){
   event.preventDefault(); //Эта строчка отменяет стандартную отправку формы.
-  switch (state){
-    case 'editButton':
-      saveFormEditButton();
-      break;
-    case 'addButton':
-      saveFormAddButton();
-      break;
-  }
-  closeForm();  //закрытие формы
-}
-
-function saveFormEditButton(){  //изм. текст профиля
   userName.textContent = inputName.value;  //записываем значение полей в профиль
   userInfo.textContent = inputInfo.value;
+
+  smoothCloseOpenPopup(formEditButton);
 }
 
-function saveFormAddButton(){  // + картинка
-  createElement(inputName.value, inputInfo.value);
+function openingAddForm(){
+  smoothCloseOpenPopup(formAddButton);
+}
+
+function saveAddForm(event){
+  event.preventDefault(); //Эта строчка отменяет стандартную отправку формы.
+  createElement(inputTitle.value, inputLink.value); //создаём картинку
+
+  smoothCloseOpenPopup(formAddButton);
+
+  inputTitle.value = ""; //обнуляем ввёденное значение
+  inputLink.value = "";
 }
 
 //кнопки
 
-editButton.addEventListener('click', () => Form('editButton'));
-addButton.addEventListener('click', () => Form('addButton'));
-form.addEventListener('reset', closeForm);
-form.addEventListener('submit', saveForm);
+editButton.addEventListener('click', openingEditForm);  //edit
+formEditButton.addEventListener('reset', () => smoothCloseOpenPopup(formEditButton));
+formEditButton.addEventListener('submit', saveEditForm);
+
+addButton.addEventListener('click', openingAddForm);  //add
+formAddButton.addEventListener('reset', () => smoothCloseOpenPopup(formAddButton));
+formAddButton.addEventListener('submit', saveAddForm);
 
 //код
 
